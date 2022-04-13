80c5da661fd1800edd169b065d3ec81b1ca75368
limunosekai <powerlsh0103@gmail.com>
2022-04-14 00:05:10 +0900

    Add image upload styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import UploadForm from "./components/UploadForm";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <div>
+    <section style={{ width: "1060px", margin: "0 auto" }}>
+      <ToastContainer />
       <h2>Image Uploader</h2>
       <UploadForm />
-    </div>
+    </section>
   );
 };
 
--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import client from "../client";
+import { toast } from "react-toastify";
+
+import ProgressBar from "./ProgressBar";
+import "./UploadForm.css";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("이미지 파일을 업로드 해주세요.");
+  const [percent, setPercent] = useState(0);
+  const [imgSrc, setImgSrc] = useState(null);
 
   const handleImageSelect = (e) => {
     const imageFile = e.target.files[0];
     setFile(imageFile);
     setFileName(imageFile.name);
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(imageFile);
+    fileReader.onload = (e) => setImgSrc(e.target.result);
   };
 
   const handleSubmit = async (e) => {
@@ -20,18 +29,41 @@ const UploadForm = () => {
         headers: {
           "Content-Type": "multipart/form-data",
         },
+        onUploadProgress: (e) => {
+          setPercent(Math.round((100 * e.loaded) / e.total));
+        },
       });
-      console.log(res);
+      toast.success("업로드 성공");
+      setTimeout(() => {
+        setPercent(0);
+        setFileName("이미지 파일을 업로드 해주세요.");
+        setImgSrc(null);
+      }, 3000);
     } catch (err) {
-      console.error(err);
+      console.error(err.message);
+      toast.error("업로드 실패");
+      setPercent(0);
+      setFileName("이미지 파일을 업로드 해주세요.");
+      setImgSrc(null);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="image">{fileName}</label>
-      <input id="image" type="file" onChange={handleImageSelect} />
-      <button type="submit">제출</button>
+      {imgSrc && <img className="image-preview" src={imgSrc} alt="uploaded" />}
+      <ProgressBar percent={percent} />
+      <div className="file-drop">
+        {fileName}
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          onChange={handleImageSelect}
+        />
+      </div>
+      <button type="submit" className="submit-btn">
+        제출
+      </button>
     </form>
   );
 };
