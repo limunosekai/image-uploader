875878dae2d47761dae53c88e413645bdaba4ffd
limunosekai <powerlsh0103@gmail.com>
2022-04-24 16:04:36 +0900

    Refector server code

--- server/main.js
+++ server/main.js
@@ -1,33 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const multer = require("multer");
-const { v4: uuid } = require("uuid");
-const mime = require("mime-types");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Image = require("./models/image");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "./uploads"),
-  filename: (req, file, cb) =>
-    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
-});
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (["image/jpeg", "image/png", "image/jpg"].includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("invalid file type"), false);
-    }
-  },
-  limits: {
-    fileSize: 1024 * 1024 * 5, // 5 MB
-  },
-});
+const { imageRouter } = require("./routes/imageRouter");
 
 const app = express();
-const PORT = 4005;
+const PORT = process.env.PORT || 4005;
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -35,17 +13,7 @@ mongoose
     console.log("MongoDB Connected...");
     app.use("/uploads", express.static("uploads"));
     app.use(cors());
-    app.post("/images", upload.single("image"), async (req, res) => {
-      const image = await new Image({
-        key: req.file.filename,
-        __filename: req.file.originalname,
-      }).save();
-      res.json(image);
-    });
-    app.get("/images", async (req, res) => {
-      const images = await Image.find();
-      res.json(images);
-    });
+    app.use("/images", imageRouter);
     app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
   })
   .catch((err) => console.error(err));
